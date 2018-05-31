69b19de44391cc40563ef4e36eea853e0472be07
oyaji <rad.kmb@gmail.com>
2018-06-01 06:59:32 +0900

    add new file

---
+++ Problem2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + 3);
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - 3);
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+        }
+    }
+}
