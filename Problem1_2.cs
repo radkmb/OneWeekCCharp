dfc76d60b68f79303039c765868f479c0bc278c2
oyaji <rad.kmb@gmail.com>
2018-06-01 06:47:07 +0900

    Add files via upload

---
+++ Problem1_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123456789");
+        }
+    }
+}
