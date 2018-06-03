9ab553b7daa39839dbf356b1d3207cb5af41ed67
oyaji <rad.kmb@gmail.com>
2018-06-03 23:59:26 +0900

    add new file

---
+++ Problem2_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2;
+
+            Console.Write("文字列１を入力:");
+            str1 = Console.ReadLine();
+            Console.Write("文字列２を入力:");
+            str2 = Console.ReadLine();
+            Console.WriteLine("文字列１＋文字列２＝{0}", str1 + str2);
+        }
+    }
+}
