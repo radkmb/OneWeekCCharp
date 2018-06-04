11867d4c590659cd605d5c8b4b66efad6aa52824
oyaji <rad.kmb@gmail.com>
2018-06-05 02:29:09 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字列１を入力:");
+            string a = Console.ReadLine();
+            Console.Write("文字列２を入力:");
+            string b = Console.ReadLine();
+
+            if (a == b)
+            {
+                Console.WriteLine("２つの文字列は等しい");
+            }
+            else 
+            {
+                Console.WriteLine("２つの文字列は等しくない");
+            }
+        }
+    }
+}
