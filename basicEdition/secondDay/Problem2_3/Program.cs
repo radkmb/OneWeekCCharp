86e3092bf9458323a8609f7ac8b0002a60ba8ac1
oyaji <rad.kmb@gmail.com>
2018-06-05 02:11:01 +0900

    add new file

---
+++ basicEdition/secondDay/Problem2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            Console.Write("文字列を入力:");
+            str = Console.ReadLine();
+            Console.WriteLine("入力された文字列:{0}", str);
+        }
+    }
+}
