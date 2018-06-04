42ecb23d4febe2dbbbe94ffd2e2828a4833110b2
oyaji <rad.kmb@gmail.com>
2018-06-05 02:18:08 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a < 50)
+            {
+                Console.WriteLine("50未満です");
+            }
+        }
+    }
+}
