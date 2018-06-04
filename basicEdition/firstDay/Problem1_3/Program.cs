efe7bf7076a96b18528bf125a2f1c3debe817cd0
oyaji <rad.kmb@gmail.com>
2018-06-05 02:03:36 +0900

    add new file

---
+++ basicEdition/firstDay/Problem1_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("こんにちは");
+        }
+    }
+}
