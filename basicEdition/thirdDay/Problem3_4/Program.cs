9fa6f7640033bb90c477e790c785a6921a5ad45f
oyaji <rad.kmb@gmail.com>
2018-06-05 02:19:30 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a <= 20 || a >= 80)
+            {
+                Console.WriteLine("20以下か80以上の値です");
+            }
+        }
+    }
+}
