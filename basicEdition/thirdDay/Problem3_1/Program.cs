9c74085df48482d4a018378ce483915477789603
oyaji <rad.kmb@gmail.com>
2018-06-05 02:16:06 +0900

    create new file

---
+++ basicEdition/thirdDay/Problem3_1/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a == 3)
+            {
+                Console.WriteLine("{0}です", a);
+            }
+        }
+    }
+}
