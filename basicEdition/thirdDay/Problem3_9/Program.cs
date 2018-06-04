6c238f44178490cb24b3497d11deaa315751f4a9
oyaji <rad.kmb@gmail.com>
2018-06-05 02:32:27 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("文字列を入力:");
+            string a = Console.ReadLine();
+
+            if (a == "abc")
+            {
+                Console.WriteLine("abcです");
+            }
+            else
+            {
+                Console.WriteLine("abcではありません");
+            }
+        }
+    }
+}
