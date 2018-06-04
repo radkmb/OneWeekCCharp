4c7e9959c4517860d11d5cbd2d2f22a9ad8be73b
oyaji <rad.kmb@gmail.com>
2018-06-05 02:25:00 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数値を入力:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("この数は偶数です。");
+            }
+            else
+            {
+                Console.WriteLine("この数は奇数です。");
+            }
+        }
+    }
+}
