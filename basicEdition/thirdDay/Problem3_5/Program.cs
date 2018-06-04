a95a3788616e190220ecfda3820377ba72d86fe9
oyaji <rad.kmb@gmail.com>
2018-06-05 02:20:31 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a > 30 && a <= 70)
+            {
+                Console.WriteLine("30より大きく70以下です");
+            }
+        }
+    }
+}
