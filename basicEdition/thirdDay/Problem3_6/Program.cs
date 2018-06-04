6fcb0d40c95ce809206af55bb4cfb8a6ee3b2f3c
oyaji <rad.kmb@gmail.com>
2018-06-05 02:22:55 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problem3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a={0} b={1}", a, b);
+
+            if (a == b)
+            {
+                Console.WriteLine("aはbと等しい");
+            }
+            else if (a > b)
+            {
+                Console.WriteLine("aはbよりも大きい");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("aはbよりも小さい");
+            }
+        }
+    }
+}
