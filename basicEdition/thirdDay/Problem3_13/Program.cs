7f2ad88711cb570b7707be957ed35addb71ad07f
oyaji <rad.kmb@gmail.com>
2018-06-06 19:18:11 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1から3の値を入力（1:グー 2:パー 3:チョキ):");
+            int num = int.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("グー");
+                    break;
+                case 2:
+                    Console.WriteLine("チョキ");
+                    break;
+                case 3:
+                    Console.WriteLine("パー");
+                    break;
+                default:
+                    Console.WriteLine("正しい値ではありません");
+                    break;
+            }
+        }
+    }
+}
