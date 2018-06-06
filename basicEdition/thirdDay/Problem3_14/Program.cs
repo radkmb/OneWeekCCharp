9033d4123d7bd51b71f7f6e7ddac140581f8b8e9
oyaji <rad.kmb@gmail.com>
2018-06-06 19:20:39 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a,bに1から10までの数値を入力してください。");
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a >= 1 && a <= 10 && b >= 1 && b <= 10)
+            {
+                if (a > b)
+                {
+                    Console.WriteLine("aのほうが大きいです。");
+                }
+                else if (a < b)
+                {
+                    Console.WriteLine("bのほうが大きいです。");
+                }
+                else
+                {
+                    Console.WriteLine("等しいです");
+                }
+            }
+            else
+            {
+                Console.WriteLine("範囲外です");
+            }
+        }
+    }
+}
