4a71c23fb37eb1115e7ba24cb5d31349872f322d
oyaji <rad.kmb@gmail.com>
2018-06-06 19:14:38 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a != 0)
+            {
+                if (a < 1)
+                {
+                    Console.WriteLine("負の値です");
+                }
+                else
+                {
+                    Console.WriteLine("正の値です");
+                }
+            }
+            else
+            {
+                Console.WriteLine("0です");
+            }
+        }
+    }
+}
