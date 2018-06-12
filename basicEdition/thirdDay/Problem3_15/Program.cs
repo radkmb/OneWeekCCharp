0eb9755ed72eac7bff11d76234941a3d9ff7548b
oyaji <rad.kmb@gmail.com>
2018-06-12 15:02:42 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("点数(1-100)：");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >= 1 && num <= 100)
+            {
+                if (num >= 80)
+                {
+                    Console.WriteLine("優");
+                }
+                else if (num < 80 && num >= 70)
+                {
+                    Console.WriteLine("良");
+                }
+                else if (num < 70 && num >= 60)
+                {
+                    Console.WriteLine("可");
+                }
+                else
+                {
+                    Console.WriteLine("不可");
+                }
+            }
+            else
+            {
+                Console.WriteLine("範囲外です");
+            }
+        }
+    }
+}
