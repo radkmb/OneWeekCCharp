56f64b8ca16653fe4883aa48a147cb1b8e2b4867
oyaji <rad.kmb@gmail.com>
2018-06-12 15:04:22 +0900

    add new file

---
+++ Problem3_16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数値を入力：");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0 && num % 3 == 0)
+            {
+                Console.WriteLine("2と3の公倍数です。");
+            }
+            else if (num % 2 == 0)
+            {
+                Console.WriteLine("2の倍数です。");
+            }
+            else if(num % 3 == 0)
+            {
+                Console.WriteLine("3の倍数です。");
+            }
+        }
+    }
+}
