01ae7cfd83e4999aad3155a481dd12b3350e62b7
oyaji <rad.kmb@gmail.com>
2018-06-13 00:28:02 +0900

    add new file

---
+++ basicEdition/thirdDay/Problem3_17/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください(1-100)：");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >= 1 && num <= 100)
+            {
+                if (num <= 50 && num % 2 == 0)
+                {
+                    Console.WriteLine("50以下の偶数です。");
+                }
+                else if (num <= 50)
+                {
+                    Console.WriteLine("50以下です。");
+                }
+                else if (num % 2 == 0)
+                {
+                    Console.WriteLine("偶数です。");
+                }
+            }
+            else
+            {
+                Console.WriteLine("適切な値を入力してください。");
+            }
+        }
+    }
+}
