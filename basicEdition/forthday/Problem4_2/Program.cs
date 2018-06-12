d670c5f4e138551331504cff53f4e144c9cb7814
oyaji <rad.kmb@gmail.com>
2018-06-13 00:51:51 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数：");
+            int num = int.Parse(Console.ReadLine());
+
+            int i = 0;
+            while (i < num)
+            {
+                Console.Write("■");
+                i++;
+            }
+        }
+    }
+}
