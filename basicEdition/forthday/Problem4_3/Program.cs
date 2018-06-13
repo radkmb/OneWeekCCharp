fa9b0d569e0c105ccffa9127f05ffa12bca9b821
oyaji <rad.kmb@gmail.com>
2018-06-13 22:04:12 +0900

    Add files via upload

---
+++ basicEdition/forthday/Problem4_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数：");
+            int num = int.Parse(Console.ReadLine());
+
+            int i = 0;
+            do
+            {
+                Console.Write("■");
+                i++;
+            }
+            while (i < num);
+        }
+    }
+}
