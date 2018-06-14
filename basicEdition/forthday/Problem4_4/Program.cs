207667e947accfb61321daa61eb83cc5100969d7
oyaji <rad.kmb@gmail.com>
2018-06-14 09:06:48 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+
+            while (num >= 0)
+            {
+                Console.WriteLine("num=" + num);
+                num--;
+            }
+        }
+    }
+}
