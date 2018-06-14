939e730a8cd50e1840d52cb72ab00d8e7a7b895b
oyaji <rad.kmb@gmail.com>
2018-06-14 09:08:02 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i >= 0; i--)
+            {
+                Console.WriteLine("num=" + i);
+            }
+        }
+    }
+}
