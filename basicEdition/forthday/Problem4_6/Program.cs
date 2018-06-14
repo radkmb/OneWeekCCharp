a3ede4c3cfb3b8075f8c89ca48f8f1c92eaece20
oyaji <rad.kmb@gmail.com>
2018-06-14 09:09:13 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            while (a <= 8)
+            {
+                while (a % 2 == 0)
+                {
+                    Console.WriteLine("num=" + a);
+                    a++;
+                }
+                a++;
+            }
+        }
+    }
+}
