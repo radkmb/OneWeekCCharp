a5bfd839f14e819f1170b34303c1899b6c5eddc0
oyaji <rad.kmb@gmail.com>
2018-06-19 11:07:14 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Write("0以上の数値を入力してください:");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(num);
+
+                if (num < 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
