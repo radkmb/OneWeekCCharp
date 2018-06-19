b1f6767c5a859e024795e38b470ab728df71a120
oyaji <rad.kmb@gmail.com>
2018-06-19 11:14:47 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem4_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int evenCount = 0;
+            int oddCount = 0;
+
+            Random rnd = new Random();
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    int a = rnd.Next(1, 101);
+                    Console.WriteLine(a);
+                    if (a % 2 == 0)
+                    {
+                        evenCount++;
+                    }
+                    else
+                    {
+                        oddCount++;
+                    }
+                }
+                Console.WriteLine("偶数は：" + evenCount);
+                Console.WriteLine("奇数は：" + oddCount);
+            }
+        }
+    }
+}
