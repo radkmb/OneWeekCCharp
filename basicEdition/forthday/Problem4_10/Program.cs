4bd819968cff88dd91a312bec48cf3dcdd533720
oyaji <rad.kmb@gmail.com>
2018-06-19 11:10:27 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int dice = rnd.Next(1, 100);
+                Console.WriteLine(dice);
+            }
+        }
+    }
+}
