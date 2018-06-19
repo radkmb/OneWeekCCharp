d54d3e67193c2954abd0724fcf3d447b2a00417b
oyaji <rad.kmb@gmail.com>
2018-06-19 11:11:45 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> rndList = new List<int>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int dice = rnd.Next(1, 100);
+                Console.WriteLine(dice);
+                rndList.Add(dice);
+            }
+            int min = rndList.Min();
+            int max = rndList.Max();
+
+            Console.WriteLine("最大値：" + max);
+            Console.WriteLine("最小値："　+ min);
+        }
+    }
+}
