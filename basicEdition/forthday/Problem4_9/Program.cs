6c7509feb134dc8292772ffd26702a8257212585
oyaji <rad.kmb@gmail.com>
2018-06-19 11:08:59 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            while (true)
+            {
+                int a = rnd.Next(1, 100);
+                Console.WriteLine(a);
+
+                if (a % 10 == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("終了します");
+        }
+    }
+}
