30050e41802bf9f21206690f141b8749bc077e56
oyaji <rad.kmb@gmail.com>
2018-06-19 11:06:01 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 2; a <= 8; a++)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("num=" + a);
+                }
+            }
+        }
+    }
+}
