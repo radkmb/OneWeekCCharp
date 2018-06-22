7674bf0151a14cb75982e66c9d2da1d17a589874
oyaji <rad.kmb@gmail.com>
2018-06-23 02:39:36 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(new string('■', i));
+                Console.Write(new string('□', 10 - i));
+                Console.WriteLine();
+            }
+        }
+    }
+}
