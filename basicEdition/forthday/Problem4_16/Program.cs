5ae64d6c4f40e7751bb74f77c10b85551c904ed2
oyaji <rad.kmb@gmail.com>
2018-06-23 02:38:32 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 1; b <= 9; b++)
+                {
+                    int c = a * b;
+                    Console.Write(a + "*" + b + "=" + c + " ");
+                }
+            }
+        }
+    }
+}
