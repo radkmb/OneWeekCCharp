9cdfffd248c183b936fb8a97a3a009fb18fcde89
oyaji <rad.kmb@gmail.com>
2018-06-23 02:37:30 +0900

    add new file

---
+++ basicEdition/forthday/Problem4_15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine(i + " ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
