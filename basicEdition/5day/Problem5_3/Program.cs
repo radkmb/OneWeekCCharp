7ac9d894e87dba29701f52be070fd1c57f457685
oyaji <rad.kmb@gmail.com>
2018-06-24 18:50:37 +0900

    add new file

---
+++ basicEdition/5day/Problem5_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] n = new int[10];
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                n[i] = rnd.Next(1, 101);
+            }
+
+            for (i = 0; i < n.Length; i++)
+            {
+                Console.Write(n[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("奇数:");
+            for (i = 0; i < n.Length; i++)
+            {
+                if (n[i] % 2 != 0)
+                {
+                    Console.Write(n[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            Console.Write("偶数:");
+            for (i = 0; i < n.Length; i++)
+            {
+                if (n[i] % 2 == 0)
+                {
+                    Console.Write(n[i] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
